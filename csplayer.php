e9b64212a484e04662b4843b641560a3a463588c
hewwodarkness <66019326+shuvaIov@users.noreply.github.com>
2021-01-05 23:41:14 +0200

    update for tablets and smartphones
    scale for every device

--- csplayer.php
+++ csplayer.php
@@ -3,6 +3,8 @@ echo "<!DOCTYPE html>
 <link rel="stylesheet" href="css/csplayer.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 
 <?php 
     include("db_conn1.php");
